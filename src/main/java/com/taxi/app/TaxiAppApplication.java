a6ac037e3c42ed45410536b5d01aa83455e6ab30
it25300125 <it25300125@my.sliit.lk>
2026-05-24 17:39:12 +0530

    feat: add application entry point and MySQL database configuration

---
+++ src/main/java/com/taxi/app/TaxiAppApplication.java
@@ -0,0 +1,27 @@
+package com.taxi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class TaxiAppApplication {
+
+	private final Environment env;
+
+	public TaxiAppApplication(Environment env) {
+		this.env = env;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiAppApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void onReady() {
+		String port = env.getProperty("server.port", "8090");
+		System.out.println(">>> GoRide app is running at http://localhost:" + port);
+	}
+}
